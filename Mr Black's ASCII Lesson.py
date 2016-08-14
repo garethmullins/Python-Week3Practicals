29af06fb7aff5211ca536260d0734500fe86e0c5
garethmullins <gareth.mullins@my.jcu.edu.au>
2016-08-15 09:32:04 +1000

    Develop the function "get_number"

--- Mr Black's ASCII Lesson.py
+++ Mr Black's ASCII Lesson.py
@@ -5,5 +5,21 @@ def main():
         print("{:<5} {}".format(code[i], chr(int(code[i]))))
 
 
+def get_number(LOWER, UPPER):
+    input_codes = input("Enter the ASCII codes between {} and {}, seperate the codes with commas \",\" ". format(LOWER, UPPER))
+    codes = input_codes.split(",")
+    errors = 1
+    while errors > 0:
+        errors = 0
+        for i in range(0, len(codes)):
+            if int(codes[i]) < LOWER or int(codes[i]) > UPPER:
+                errors += 1
+        if errors > 0:
+            print("An invalid code was entered.")
+            input_codes = input("Enter the ASCII codes between {} and {}, seperate the codes with commas \",\" ". format(LOWER, UPPER))
+            codes = input_codes.split(",")
+    return codes
+
+
 if __name__ == "__main__":
         main()
